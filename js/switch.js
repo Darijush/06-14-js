1befd630c5a68d8e75ae94509622e1c8cac12b59
Darijush Prokopovich <darijus.pc@gmail.com>
2022-06-14 11:45:39 +0300

    switch

---
+++ js/switch.js
@@ -0,0 +1,12 @@
+const day = 1;
+switch (day) {
+    case 1:
+        console.log ('pirmadienis');
+        break;
+     case 2:
+        console.log ('antradienis');
+        break;
+     case 3:
+        console.log ('treciadienis');
+        break;
+}
